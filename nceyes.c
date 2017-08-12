a0d59cbc351dce635f524fb44f29075a38dd311e
Nik Nyby <nikolas@gnu.org>
2017-08-11 21:57:35 -0400

    respond to mouse click

--- nceyes.c
+++ nceyes.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ncurses.h>
 #include <math.h>
+#include <ncurses.h>
 
-#define DEGTORAD(deg) (deg * (180.0f/M_PI))
+#define DEGTORAD(deg) (deg * (180.0f / M_PI))
 #define DOT 46
 
-void draweye(int y, int x) {
+/**
+ * poffy / poffx: pupil offsets
+ */
+void draweye(int y, int x, int poffy, int poffx) {
     int xx, yy;
     float deg;
     int width = 5;
     int height = 5;
 
-    mvaddch(y, x, 'o');
+    // pupils
+    mvprintw(y + poffy, x + poffx, "ø");
+
+    // eyes
     for (deg = 0; deg < 360.0f; deg += 1.0f) {
         xx = (int)(width * cos(DEGTORAD(deg))) + x;
         yy = (int)(height * sin(DEGTORAD(deg))) + y;
@@ -23,23 +29,45 @@ void draweye(int y, int x) {
 }
 
 int main(int argc, char **argv) {
-    char mesg[] = "O_o";
-    int row, col;
+    int ch;
+    int row, col, x, y;
+    MEVENT event;
 
     if (!initscr()) {
         exit(1);
     };
 
+    raw();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    noecho();
+
+    clear();
+    cbreak();
+
     getmaxyx(stdscr, row, col);
 
-    int y = row / 2;
-    int x = (col - strlen(mesg)) / 2;
-    draweye(y, x - 10);
-    draweye(y, x + 10);
+    y = row / 2;
+    x = col / 2;
+    draweye(y, x - 10, 0, 0);
+    draweye(y, x + 10, 0, 0);
 
-    refresh();
-    getch();
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+
+    while ((ch = getch()) != 'q') {
+        if (ch == KEY_MOUSE) {
+            if (getmouse(&event) == OK) {
+                if (event.bstate && BUTTON1_PRESSED) {
+                    clear();
+                    draweye(y, x - 10, -2, -2);
+                    draweye(y, x + 10, -2, -2);
+                }
+            }
+        }
+        refresh();
+    }
+
     endwin();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
